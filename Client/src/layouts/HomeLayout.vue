4e30408bb8fe44da787d5d2475531dfdb3cc136a
Lucas Tomkow <49009354+lucas-pohl-tomko@users.noreply.github.com>
2022-03-04 10:59:18 -0300

    Teste de estilização para o layout principal :sparkles:

--- Client/src/layouts/HomeLayout.vue
+++ Client/src/layouts/HomeLayout.vue
@@ -43,35 +43,49 @@ export default {};
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    background-color:#e3e3e3 ;
 }
+
+.navBar {
+    margin: .2rem;
+    background-color: #bae8e8;
+    position: absolute;
+    width: calc(100% - 15.2rem);
+    height: 4rem;
+    border-radius:1rem 1rem 1rem 1rem;
+
+}
+
+.sidebarSizedMargin{
+    margin-left: 15.2rem;
+}
+
+.homeIcon{
+    margin: .2rem;
+    position: absolute;
+    background-image: linear-gradient(180deg, #2c698d, #272643);
+    height: 4rem;
+    padding-left: 6.1525rem;
+    padding-right: 6.1525rem;
+    border-radius:1.2rem 1.2rem 0rem 0rem;
+
+}
+
 .sidebar {
+    margin: .2rem;
     width: 15rem;
-    height: calc(100% - 4rem);
+    border-radius:0 0 1rem 1rem;
+    height: calc(100% - 4.2rem);
     position: absolute;
     margin-top: 4rem;
     background-color: #272643;
 }
-.navBar {
-    background-color: #bae8e8;
-    position: absolute;
-    width: calc(100% - 15rem);
-    height: 4rem;
-}
-.sidebarSizedMargin{
-    margin-left: 15rem;
-}
-.homeIcon{
-    /* margin:0 5.6rem 0 5.6rem; */
-    position: absolute;
-    background-color: #2c698d;
-    height: 4rem;
-    padding-left: 6.2rem;
-    padding-right: 6.2rem;
-}
+
+
 .content {
-    height: calc(100vh - 4rem);
-    margin-top: 4rem;
-    margin-left: 15rem;
+    height: calc(100vh - 4.2rem);
+    margin-top: calc(4rem + .2rem);
+    margin-left: calc(15rem + .2rem);
     background-color: #e3e3e3;
     overflow-y: auto;
 }
